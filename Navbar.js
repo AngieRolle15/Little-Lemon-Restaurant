ed2e7876f4fa7253a524015276abaea9fc4ad083
AngieRolle15 <ataminisahmita@hotmail.com>
2023-07-04 22:09:31 -0400

    Changes made to meta tags and OGP tags

---
+++ Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./src/images/Logo .svg";
+
+return (
+<>
+    <img src={logo} alt="logoofLittleLemon"></img>
+<nav className="nav">
+     <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+    </ul>
+    </nav>
+    </>
+)
+
+export default Navbar;
